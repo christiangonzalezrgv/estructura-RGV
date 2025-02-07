2ee477b6edf73f80dd6159717571e3ca96fde2d5
grchristian <a01657929@tec.mx>
2025-02-07 13:23:37 -0600

    estructura v1.12

--- app/urls.py
+++ app/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     path("documentacion/", DocumentacionView.as_view(), name="documentacion"),
     path("auditoria/", AuditoriaView.as_view(), name="auditoria"),
     # Ruta para el login
-    path("authentication/login/", UserLoginView.as_view(), name="login"),
+    path("auth/login/", UserLoginView.as_view(), name="login"),
     # Ruta para el logout
-    path("authentication/logout/", LogoutView.as_view(), name="logout"),
+    path("auth/logout/", LogoutView.as_view(), name="logout"),
+    # implementar forgot password
 ]
