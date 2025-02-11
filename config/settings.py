64b6c44eca1c7c0825cc4db7da36eaa76bc1916d
christiangonzalezrgv <christian.gonzalez@rgvsoluciones.com>
2025-02-11 17:21:42 -0600

    trigger build v13

--- config/settings.py
+++ config/settings.py
@@ -49,7 +49,7 @@ DEBUG = os.getenv("DEBUG", "True") == "True"
 APP_NAME = os.getenv("APP_NAME", "default_app").lower()
 
 # Hosts permitidos
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "127.0.0.1,localhost,estructura-django-dev.us-east-2.elasticbeanstalk.com").split(",")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",") if os.getenv("ALLOWED_HOSTS") else []
 
 # Aplicaciones instaladas
 INSTALLED_APPS = [
