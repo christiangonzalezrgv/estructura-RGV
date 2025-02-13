f9f3142148adb4ffdecc3a3d2b3a75eabe968841
christiangonzalezrgv <christian.gonzalez@rgvsoluciones.com>
2025-02-13 11:26:25 -0600

    trigger

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
 # Inicia Gunicorn para servir la aplicación en el puerto 80
-exec gunicorn config.wsgi:application --bind 0.0.0.0:80
+exec gunicorn config.wsgi:application --bind 0.0.0.0:8000
